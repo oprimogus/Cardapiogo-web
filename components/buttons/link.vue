9f82266039b832a88caa05ca72e7f0665b1e086a
Gustavo Ferreira <gustavo.jesus@proton.me>
2024-03-11 02:20:21 -0300

    feat: add header

---
+++ components/buttons/link.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLink :to="to" :class="buttonClass">
+    <slot></slot> <!-- Permite a inserção de conteúdo customizado -->
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+    variant: {
+      type: String,
+      default: 'default',
+    },
+  },
+  computed: {
+    buttonClass() {
+      const baseClasses = 'p-[0.5vw] font-bold rounded-md flex flex-row justify-center items-center ';
+      if (this.variant === 'secondary') {
+        return `${baseClasses} bg-secondary text-primary`;
+      }
+      return `${baseClasses} text-secondary`;
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <headerHome></headerHome>
+</template>
