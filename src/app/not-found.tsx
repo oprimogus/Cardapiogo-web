2f5ca0a5f54489aff54d05c7dc1bb562777e6fad
oprimogus <gustavo.jesus@proton.me>
2024-07-27 19:55:05 -0300

    fix: not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
         <section className="bg-red-600 min-h-screen flex items-center justify-center p-4">
             <div className="h-3/4 w-3/4 lg:h-2/3 lg:w-2/3 xl:h-1/2 xl:w-1/2 bg-white rounded-lg shadow-md flex flex-col items-center justify-center p-6">
-                <img src="./ilustrations/undraw_page_not_found_re_e9o6.svg" alt="" className="w-3/4 h-3/4"/>
+                <img src="/ilustrations/undraw_page_not_found_re_e9o6.svg" alt="" className="w-3/4 h-3/4"/>
                 
                 <span className="text-md m-4 font-medium text-black uppercase text-center">
                     Página não encontrada
