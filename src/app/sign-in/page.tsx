eba8676adc8ae4a8d9f7c60a04af874e71cfc636
oprimogus <gustavo.jesus@proton.me>
2024-07-27 19:54:54 -0300

    feat: sign-in page

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import { FooterHome } from '@/components/footer/footerHome';
+import { SignIn } from '@/components/forms/sign-in';
+import { HeaderHome } from '@/components/header/headerHome';
+
+export default function SignInPage () {
+  return (
+    <main>
+        <HeaderHome/>
+        <SignIn/>
+        <FooterHome/>
+    </main>
+  );
+}
